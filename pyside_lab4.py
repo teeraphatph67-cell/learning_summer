cee66c1083c3d674f0c538e8b86b24fd6486998e
teeraphatph67-cell <teeraphat.ph.67@ubu.ac.th>
2026-05-02 23:28:17 +0700

    Update pyside_lab4.py
    การจัดโครงสร้างแบบ OOP (Class) และการใช้งาน Layout Manager

--- pyside_lab4.py
+++ pyside_lab4.py
@@ -1,13 +1,16 @@
 import sys
 from PySide6.QtWidgets import *
-app = QApplication(sys.argv)
-win = QWidget()
-win.resize(320, 240) #กาํหนดขนาดของหนา้ตา่ งโปรแกรม
-win.setWindowTitle("Hello, World!") #กาํหนดชืÉอตรงหัวโปรแกรม
-layout = QVBoxLayout() #ดึง QVBoxLayout() มาใชง้านเพิÉมให้เราสามารถดึงวตัถุมารวมกนั ได้
-win.setLayout(layout) #กาํหนดให้ตวัแปร win ชึÉงเป็ น QWidget ดึงตวัแปรทีÉอยใู่ น layout มาแสดง
-label = QLabel("Hello World") #กาํหนดขอ้ความ
-layout.addWidget(label) #เพิÉมดึงตวัแปร label เขา้มา
-win.show() #แสดง
-app.exec()
-sys.exit()
+class myApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(320, 240) # กาํหนดขนาดของหนา้ต่างโปรแกรม
+        self.setWindowTitle("Hello, World!") # กาํหนดชืÉอตรงหัวโปรแกรม
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        label = QLabel("Hello World")
+        layout.addWidget(label)
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = myApp()
+    win.show()
+    sys.exit(app.exec())

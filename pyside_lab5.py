6b28464a1fcbd5d2172b968072232e280583393d
teeraphatph67-cell <teeraphat.ph.67@ubu.ac.th>
2026-05-02 23:30:25 +0700

    Update pyside_lab5.py
    การจัดวางข้อความหลายชุด (Multiple Labels) ด้วย QVBoxLayout ใน PySide6 โดยใช้โครงสร้างแบบ Class

--- pyside_lab5.py
+++ pyside_lab5.py
@@ -7,4 +7,16 @@ class myApp(QWidget):
         self.setWindowTitle("Hello, World!") # กาํหนดชืÉอตรงหัวโปรแกรม
         layout = QVBoxLayout()
         self.setLayout(layout)
+
         label = QLabel("Hello World")
+
+        label2 = QLabel("สวสัดี")
+        label3 = QLabel("เพิÉงเริÉมตน้ เรียน PySide -..- จากผเู้ขียนบทความ")
+        layout.addWidget(label)
+        layout.addWidget(label2)
+        layout.addWidget(label3)
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = myApp()
+    win.show()
+    sys.exit(app.exec())
